017f45044c04df063623e92948dcd3407e0bbff7
Sergi Castillo <sergi.castillo@scmspain.com>
2019-06-21 10:31:29 +0200

    Paint path

--- app/src/main/java/com/karumi/androidanimations/pathdrawing/PathDrawingFragment.kt
+++ app/src/main/java/com/karumi/androidanimations/pathdrawing/PathDrawingFragment.kt
@@ -1,11 +1,16 @@
 package com.karumi.androidanimations.pathdrawing
 
+import android.graphics.Color
+import android.graphics.Path
+import android.graphics.RectF
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.karumi.androidanimations.R
 import com.karumi.androidanimations.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_path_drawing.*
 
 class PathDrawingFragment : BaseFragment() {
     override fun onCreateView(
@@ -16,15 +21,23 @@ class PathDrawingFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        TODO(
-            """
-            |Draw the exercise path on screen. You can use absolute coordinates or relative ones.
-            |If you follow the relative path (pwn intended), you can use DisplayMetrics:
-            |
-            |  val displayMetrics = DisplayMetrics()
-            |  requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
-            |  displayMetrics.widthPixels
-        """.trimMargin()
-        )
+
+        val displayMetrics = DisplayMetrics()
+        requireActivity().windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val widthPixels = displayMetrics.widthPixels.toFloat()
+        val heightPixels = widthPixels
+
+        val w2 = widthPixels / 2
+        val h2 = heightPixels / 2
+
+        val apply = Path().apply {
+            moveTo(w2, 0f)
+            addArc(RectF(w2, -h2, widthPixels + w2, h2), -180f, -90f)
+            lineTo(w2, heightPixels)
+            quadTo(w2, h2, 0f, h2)
+            lineTo(w2, 0f)
+        }
+
+        paintView += apply to Color.RED
     }
 }
